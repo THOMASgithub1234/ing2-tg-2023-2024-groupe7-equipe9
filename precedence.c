0f10ad2edae2d8a4b0f569be93acfcc40d95afd8
romainarc <arcisromain@gmail.com>
2023-11-23 17:21:15 +0100

    code du graphe de precedence

--- precedence.c
+++ precedence.c
@@ -8,11 +8,34 @@
 #include <stdio.h>
 #define MAX_NODES 100 // Nombre maximal de nœuds dans le graphe
 
-void lireContraintes(char *nomFichier, int graphe[MAX_NODES][MAX_NODES]) {
+void lireContraintes(char *precedence, int graphe[MAX_NODES][MAX_NODES]) {
     FILE *file = fopen("precedences.txt", "r");
     if (file == NULL) {
         printf("Erreur lors de l'ouverture du fichier.\n");
         exit(1);
     }
 
+    int a, b;
+    for (int i = 0; i < MAX_NODES; ++i) {
+        for (int j = 0; j < MAX_NODES; ++j) {
+            graphe[i][j] = 0;
+        }
+    }
 
+    while (fscanf(file, "%d %d", &a, &b) == 2) {
+        graphe[a - 1][b - 1] = 1;
+    }
+
+    fclose(file);
+}
+
+void afficherGraphe(int graphe[MAX_NODES][MAX_NODES]) {
+    printf("Graphe de precedance :\n");
+    for (int i = 0; i < MAX_NODES; ++i) {
+        for (int j = 0; j < MAX_NODES; ++j) {
+            if (graphe[i][j] == 1) {
+                printf("%d -> %d\n", i + 1, j + 1);
+            }
+        }
+    }
+}
--- precedence.h
+++ precedence.h
@@ -5,4 +5,12 @@
 #ifndef ING2_TG_2023_2024_GROUPE7_EQUIPE9_PRECEDENCE_H
 #define ING2_TG_2023_2024_GROUPE7_EQUIPE9_PRECEDENCE_H
 
-#endif //ING2_TG_2023_2024_GROUPE7_EQUIPE9_PRECEDENCE_H
+
+
+
+#define MAX_NODES 100
+
+void lireContraintes(char *precedence, int graphe[MAX_NODES][MAX_NODES]);
+void afficherGraphe(int graphe[MAX_NODES][MAX_NODES]);
+
+#endif /
