9e80b1ba7f516d587812476d371dc4cbea7c7a98
romainarc <arcisromain@gmail.com>
2023-11-23 17:18:49 +0100

    ouverture du fichier precedences.txt

--- precedence.c
+++ precedence.c
@@ -3,3 +3,16 @@
 //
 
 #include "precedence.h"
+#include <stdlib.h>
+
+#include <stdio.h>
+#define MAX_NODES 100 // Nombre maximal de nœuds dans le graphe
+
+void lireContraintes(char *nomFichier, int graphe[MAX_NODES][MAX_NODES]) {
+    FILE *file = fopen("precedences.txt", "r");
+    if (file == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        exit(1);
+    }
+
+
