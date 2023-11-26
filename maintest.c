43c994e3438ce539739ce1e6896275a344455c00
Xelou76 <95855803+Xelou76@users.noreply.github.com>
2023-11-26 20:22:13 +0100

    Create maintest.c

---
+++ maintest.c
@@ -0,0 +1,29 @@
+#include "exclusionstest.h"
+
+int main() {
+    // Déclaration et initialisation des variables
+    Exclusion *contraintes;
+  
+    int nbContraintes;
+
+    // Lecture des contraintes à partir du fichier
+    lireContraintes("exclusions.txt", &contraintes, &nbContraintes);
+
+    // Obtention du nombre de stations nécessaires et des opérations par station
+    Station *stations = NULL;
+    int nbStations;
+    obtenirStations(contraintes, nbContraintes, &stations, &nbStations);
+
+    // Affichage du résultat
+    printf("Nombre de stations necessaires : %d\n", nbStations);
+    afficherStations(stations, nbStations);
+
+    // Libération de la mémoire allouée
+    free(contraintes);
+    for (int i = 0; i < nbStations; i++) {
+        free(stations[i].operations);
+    }
+    free(stations);
+
+    return 0;
+}
