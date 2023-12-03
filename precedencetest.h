c72504d44e048fb75acd837bf795f15cda679c65
Xelou76 <95855803+Xelou76@users.noreply.github.com>
2023-12-03 15:51:31 +0100

    Update precedencetest.h

--- precedencetest.h
+++ precedencetest.h
@@ -1,10 +1,9 @@
 //
-// Created by arcis on 03/12/2023.
+// Created by Arcis/Gabriel on 03/12/2023.
 //
 
 #ifndef ING2_TG_2023_2024_GROUPE7_EQUIPE9_PRECEDENCETEST_H
 #define ING2_TG_2023_2024_GROUPE7_EQUIPE9_PRECEDENCETEST_H
-
 
 #define TACHEMAX 36 // Nombre maximal de tâches
 
@@ -15,7 +14,7 @@ struct Task {
     int critical;
 };
 
-float pert(int graph[TACHEMAX][TACHEMAX], struct Task tache[TACHEMAX], int num_tasks);
+float pert(int graph[TACHEMAX][TACHEMAX], struct Task tache[TACHEMAX], int num_tasks, int criticalPath[TACHEMAX]);
 int mainPrecedence();
 void afficherCheminCritique(struct Task tache[TACHEMAX], int num_tasks, int criticalPath[TACHEMAX]);
 
